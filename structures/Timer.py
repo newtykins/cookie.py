101457967035749614c5dd6247aa2ab766e0fb75
newt! <newtykins@outlook.com>
2021-09-25 11:23:42 +0100

    Ensure all threads are killed when program exits

--- structures/Timer.py
+++ structures/Timer.py
@@ -1,6 +1,10 @@
 import threading
 
 class Timer(threading.Timer):
+	def __init__(self, interval, function):
+		super().__init__(interval, function)
+		self.daemon = True
+
 	def run(self):
 		while not self.finished.wait(self.interval):
 			self.function(*self.args, **self.kwargs)
