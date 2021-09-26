eedefc9beff975e692407e5de1f70e8b42d5b48a
newt! <newtykins@outlook.com>
2021-09-26 21:40:53 +0100

    Begin to develop a nicer GUI

--- Cookie.py
+++ Cookie.py
@@ -1,27 +1,41 @@
 from structures import GameManager, gui
-from tkinter import Label
+from tkinter import Button, Label, PhotoImage
+import os
+import ctypes
+
+user32 = ctypes.windll.user32
+screenSize = user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)
+assetsDir = os.path.dirname(os.path.realpath(__file__)) + '/assets/'
 
 # Intitate the window, stores, and menu
-window = gui.Window('Cookie Clicker', '750x750')
+window = gui.Window('Cookie Clicker', '%ix%i+0+0' % (screenSize[0] - 250, screenSize[1] - 250), '#d38808')
 manager = GameManager()
 menuBar = gui.Menu(window, manager)
 window.config(menu=menuBar)
 
 # Bind a label to the cookie store and if you click the label increment the CPS
+cookieImage = PhotoImage(file=assetsDir + 'cookie.png')
+cookieButton = Button(window, image=cookieImage, pady=50)
+cookieButton.bind('<1>', lambda e: manager.cookies.add())
 cookieLabel = Label(window, textvariable=manager.cookies.output)
-cookieLabel.bind('<Button-1>', lambda e: manager.cookies.add())
-cookieLabel.pack()
 
 # Bind a label to each generator store and if you click the label, buy one of that generator
-for generator in manager.generators.values():
-	generatorLabel = Label(window, textvariable=generator.output)
-	generatorLabel.bind('<Button-1>', lambda e: generator.buy())
-	generatorLabel.pack()
+# generatorButtons = []
+# generatorLabels = []
+
+# for generator in manager.generators.values():
+# 	generatorLabel = Label(window, textvariable=generator.output)
+# 	generatorLabel.bind('<Button-1>', lambda e: generator.buy())
 
 # Bind a label to the cookie store's CPS and if you click the label increment the CPS
 cpsLabel = Label(window, textvariable=manager.cookies.cpsOutput)
 cpsLabel.bind('<Button-1>', lambda e: manager.cookies.addCPS())
-cpsLabel.pack()
 
 # Start the window
+window.components = [
+	(cookieButton, 0, 50),
+	(cookieLabel, 0, 0, ("Calibri", 24)),
+	(cpsLabel, 0, 0, ("Calibri", 18))
+]
+
 window.start()
--- structures/gui/Window.py
+++ structures/gui/Window.py
@@ -1,10 +1,28 @@
-from tkinter import Tk
+from tkinter import Button, Label, Tk
 
 class Window(Tk):
-	def __init__(self, title: str, geometry: str):
+	def __init__(self, title: str, geometry: str, bgColour: str):
 		super().__init__()
 		self.title(title)
 		self.geometry(geometry)
+		self.bgColour = bgColour
+		self.components = []
 
 	def start(self):
+		# Pack the components with their individual configs
+		# ( component, padding x, padding y )
+		# if Label, 4th value of tuple is another tuple containing font info
+		# ( fontSize )
+		for c in self.components:
+			component = c[0]
+			padX = c[1]
+			padY = c[2]
+			if isinstance(component, Button):
+				component.configure(highlightthickness=0, bd=0, activebackground=self.bgColour)
+			elif isinstance(component, Label):
+				labelConfig = c[3]
+				component.configure(font=(labelConfig[0], labelConfig[1]))
+			component.configure(bg=self.bgColour)
+			component.pack(padx=padX, pady=padY)
+		self.configure(bg=self.bgColour)
 		self.mainloop()
